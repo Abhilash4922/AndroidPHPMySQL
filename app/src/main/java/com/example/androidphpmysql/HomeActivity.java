fd1b9d269154d999e57261e0e81d14da3bda5c0b
Abhilash4922 <abhilashsonu11@gmail.com>
2019-05-06 19:40:08 +0530

    home page working

---
+++ app/src/main/java/com/example/androidphpmysql/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.androidphpmysql;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.homemenu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSignUp:
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+            case R.id.menuLogIn:
+                startActivity(new Intent(this,LoginActivity.class));
+                break;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/example/androidphpmysql/MainActivity.java
+++ app/src/main/java/com/example/androidphpmysql/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.androidphpmysql;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -8,17 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,7 +30,7 @@ private TextView textViewLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
 
         editTextEmail=(EditText)findViewById(R.id.editTextEmail);
         editTextName=(EditText)findViewById(R.id.editTextName);
