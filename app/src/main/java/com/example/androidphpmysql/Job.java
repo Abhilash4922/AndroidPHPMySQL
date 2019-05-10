7f92b097fbf7d7e83263a1175c5262b1c9e29af1
Abhilash4922 <abhilashsonu11@gmail.com>
2019-05-10 19:26:10 +0530

    working on job apply

--- app/src/main/java/com/example/androidphpmysql/Job.java
+++ app/src/main/java/com/example/androidphpmysql/Job.java
@@ -1,16 +1,22 @@
 package com.example.androidphpmysql;
 
 public class Job {
+
     private String title;
     private String description;
     private String link;
+    private int userId;
 
     public Job(String title, String description, String link) {
+
         this.title = title;
         this.description = description;
         this.link = link;
+        this.userId=userId;
     }
-
+    public int getUserId(){
+        return userId;
+    }
     public String getTitle() {
         return title;
     }
--- app/src/main/java/com/example/androidphpmysql/SharedPrefManager.java
+++ app/src/main/java/com/example/androidphpmysql/SharedPrefManager.java
@@ -14,7 +14,10 @@ public class SharedPrefManager {
     private static final String KEY_USER_EMAIL = "useremail";
     private static final String KEY_USER_ID = "userid";
     private static final String KEY_USER_RESUME = "resume";
-
+    private static final String KEY_JOB_ID ="jid" ;
+    private static final String KEY_USER_JOB_ID ="user_job_id" ;
+    private static final String KEY_USER_JOB_NAME ="job_name" ;
+    private static final String SHARED_PREF_NAME_JOB = "mysharedprefjob";
 
     private SharedPrefManager(Context context) {
         mCtx = context;
@@ -24,6 +27,7 @@ public class SharedPrefManager {
     public static synchronized SharedPrefManager getInstance(Context context) {
         if (mInstance == null) {
             mInstance = new SharedPrefManager(context);
+
         }
         return mInstance;
     }
@@ -36,6 +40,20 @@ public class SharedPrefManager {
         editor.putInt(KEY_USER_ID, id);
         editor.putString(KEY_USER_EMAIL, email);
         editor.putString(KEY_USERNAME, username);
+
+        editor.apply();
+
+        return true;
+    }
+
+    public boolean applliedJob(int jid,int userid, String job_name){
+
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME_JOB, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_JOB_ID, String.valueOf(jid));
+        editor.putInt(KEY_USER_JOB_ID, userid);
+        editor.putString(KEY_USER_JOB_NAME, job_name);
+
 
         editor.apply();
 
@@ -78,4 +96,19 @@ public class SharedPrefManager {
         SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         return sharedPreferences.getString(KEY_USER_RESUME, null);
     }
+    public String getKeyJobId(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME_JOB, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_JOB_ID, null);
+    }
+    public String getKeyUserJobId(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME_JOB, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_USER_JOB_ID, null);
+    }
+    public String getKeyUserJobName(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME_JOB, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_USER_JOB_NAME, null);
+    }
+
+
+
 }
